a05527a6bd8bf23ed2e2e9c3c689debc4df23dcb
Ingus Skaistkalns <ingus.skaistkalns@mitigate.dev>
2025-04-17 16:13:46 +0300

    Remove redundant require statements and do better assign of local variables

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-const { Headers } = require("node-fetch");
-const gadm = require('@google-ads/admanager').v1
+const { v1: gadm, protos} = require('@google-ads/admanager')
 
 const credentials = {
   client_email: process.env.GOOGLE_ADS_MANAGER_EMAIL,
@@ -10,14 +9,15 @@ const reportClient = new gadm.ReportServiceClient({credentials})
 
 const networkPrefix = `networks/${process.env.GOOGLE_ADS_MANAGER_NETWORK_CODE}`
 
+const { google } = protos;
 
 reportClient.createReport({
   parent: networkPrefix,
-  report: {
+  report: google.ads.admanager.v1.Report.fromObject({
     name: "TEST CREATE REPORT",
     reportDefinition: {
       dimensions: ["DATE", "AD_UNIT_ID"],
       metrics: ["ADSENSE_AVERAGE_ECPM"],
     }
-  }
+  })
 }).then(x => console.log(x))
