a38b11d41889c7fa1c159915451f50f6f09de5c8
Ingus Skaistkalns <ingus.skaistkalns@mitigate.dev>
2025-05-30 17:46:06 +0300

    Validate that credentials are correct by listing report ids

--- src/main.js
+++ src/main.js
@@ -5,19 +5,29 @@ const credentials = {
   private_key: process.env.GOOGLE_ADS_MANAGER_PRIVATE_KEY,
 }
 
+console.log(credentials)
+
 const reportClient = new gadm.ReportServiceClient({credentials})
 
 const networkPrefix = `networks/${process.env.GOOGLE_ADS_MANAGER_NETWORK_CODE}`
 
 const { google } = protos;
 
-reportClient.createReport({
-  parent: networkPrefix,
-  report: google.ads.admanager.v1.Report.fromObject({
-    name: "TEST CREATE REPORT",
-    reportDefinition: {
-      dimensions: ["DATE", "AD_UNIT_ID"],
-      metrics: ["ADSENSE_AVERAGE_ECPM"],
-    }
+reportClient.listReports({parent: networkPrefix}).then((response) => {
+  response[0].forEach((report) => {
+    console.log("Report ID: ", report.reportId)
   })
-}).then(x => console.log(x))
+
+  reportClient.createReport({
+    parent: networkPrefix,
+    report: google.ads.admanager.v1.Report.fromObject({
+      name: "TEST CREATE REPORT",
+      reportDefinition: {
+        dimensions: ["DATE", "AD_UNIT_ID"],
+        metrics: ["ADSENSE_AVERAGE_ECPM"],
+      }
+    })
+  }).then((x) => console.log(x))
+
+})
+
